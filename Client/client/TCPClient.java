f091f4cc9b03c8296425055dca8a4835b47a31cf
ZEAMARI MOHAMED <moh.67.100@gmail.com>
2018-02-26 14:40:13 +0100

    add plugin

--- Client/client/TCPClient.java
+++ Client/client/TCPClient.java
@@ -5,50 +5,50 @@ package client;
  */
 
 /**
- *
  * @author Peter Altenberd
  * (Translated into English by Ronald Moore)
  * Computer Science Dept.                   Fachbereich Informatik
  * Darmstadt Univ. of Applied Sciences      Hochschule Darmstadt
  */
 
+import org.json.JSONObject;
+
 import java.io.*;
 import java.net.*;
 
 public class TCPClient {
 
-  static String line;
-  static Socket socket;
-  static BufferedReader fromServer;
-  static DataOutputStream toServer;
-  static UserInterface user = new UserInterface();
+    static String line;
+    static Socket socket;
+    static BufferedReader fromServer;
+    static DataOutputStream toServer;
+    static UserInterface user = new UserInterface();
 
-  public static void main(String[] args) throws Exception {
-    socket = new Socket("172.17.2.0", 9999);
-    toServer = new DataOutputStream(     // Datastream FROM Server
-      socket.getOutputStream());
-    fromServer = new BufferedReader(     // Datastream TO Server
-      new InputStreamReader(socket.getInputStream()));
-    while (sendRequest()) {              // Send requests while connected
-      receiveResponse();                 // Process server's answer
+    public static void main(String[] args) throws Exception {
+        socket = new Socket("172.17.2.0", 9999);
+        toServer = new DataOutputStream(     // Datastream FROM Server
+                socket.getOutputStream());
+        fromServer = new BufferedReader(     // Datastream TO Server
+                new InputStreamReader(socket.getInputStream()));
+        while (sendRequest()) {              // Send requests while connected
+            receiveResponse();                 // Process server's answer
+        }
+        socket.close();
+        toServer.close();
+        fromServer.close();
     }
-    socket.close();
-    toServer.close();
-    fromServer.close();
-  }
 
-  private static boolean sendRequest() throws IOException {
-    boolean holdTheLine = true;          // Connection exists
-    user.output("Enter message for the Server, or end the session with . : ");
-    toServer.writeBytes((line = user.input()) + '\n');
-    if (line.equals(".")) {              // Does the user want to end the session?
-      holdTheLine = false;
+    private static boolean sendRequest() throws IOException {
+        boolean holdTheLine = true;          // Connection exists
+    
+
+        toServer.writeBytes((line = user.input()) + '\n');
+
+        return holdTheLine;
     }
-    return holdTheLine;
-  }
 
-  private static void receiveResponse() throws IOException {
-    user.output("Server answers: " +
-      new String(fromServer.readLine()) + '\n');
-  }
+    private static void receiveResponse() throws IOException {
+        user.output("Server answers: " +
+                new String(fromServer.readLine()) + '\n');
+    }
 }
