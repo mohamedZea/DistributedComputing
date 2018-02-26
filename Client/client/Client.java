f18e5e88dfb556ad477ca9a84a722cbe68c4d04a
ZEAMARI MOHAMED <moh.67.100@gmail.com>
2018-02-26 15:05:58 +0100

    add stocks file in client

---
+++ Client/client/Client.java
@@ -0,0 +1,6 @@
+package client;
+
+public class Client {
+   public String name;
+   public int id_client;
+}
---
+++ Client/client/Stocks.java
@@ -0,0 +1,8 @@
+package client;
+
+public class Stocks {
+    public StockType _type;
+    public String _code;
+    public int _amount;
+    public int _unitPrice;
+}
