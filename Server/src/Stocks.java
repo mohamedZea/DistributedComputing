6e84f5b17d286f14eba6f31cc38dffdfc487a5c0
Loïc Bine <loic.bine@gmail.com>
2018-02-26 14:58:38 +0100

    Added GSON

--- Server/src/Stocks.java
+++ Server/src/Stocks.java
@@ -1,6 +1,21 @@
-public class Stocks {
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class Stocks{
     public StockType _type;
     public String _code;
     public int _amount;
-    public int _unitPrice;
+    public double _unitPrice;
+
+    public static String Serialize(Stocks obj){
+        Gson gs = new Gson();
+        return gs.toJson(obj);
+    }
+
+    public static Stocks Deserialize(String obj){
+        Gson gs = new Gson();
+        return gs.fromJson(obj,Stocks.class);
+    }
+
 }
+
